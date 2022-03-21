d5ad32d1850af80c6782a54a88bc4b74bfd2525f
lohyingxin <65619666+lohyingxin@users.noreply.github.com>
2022-03-21 13:45:11 +0800

    buyer landing page

--- src/views/AuthHome.vue
+++ src/views/AuthHome.vue
@@ -15,11 +15,10 @@
       </router-link>
     </div>
     <div v-else class="landing-rectangle">
-      <strong class="supporting-header"
-        >Welcome back to Homebase!</strong
-      >
+      <strong class="supporting-header">Welcome back to Homebase!</strong>
       <p class="supporting-text">
-        Lorem ipsum blabla some quotes
+        With over 100 reputable small local food businesses on our platform,
+        there is sure to be an option for you.
       </p>
       <router-link
         :to="{ name: 'Search' }"
@@ -28,15 +27,113 @@
         >View All Available Bakes!</router-link
       >
     </div>
+    <v-container class="separator"></v-container>
+    <div class="browse-new">
+      <div class="content-buyer">
+        <br />
+        <h1 class="div-title">See what's new</h1>
+        <br />
+        <v-row>
+          <v-col v-for="result in getListings" :key="result.name" cols="3">
+            <v-hover>
+              <template v-slot:default="{ hover }">
+                <v-card
+                  class="rounded-lg"
+                  min-width="150"
+                  min-height="100"
+                  height="350"
+                  :to="'/listing/' + result.storeName + '/' + result.docID"
+                  hover
+                >
+                  <v-img
+                    gradient="to bottom, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0) 50%, rgba(132, 131, 131, 0.8) 100%"
+                    class="white--text align-end bottom-gradient"
+                    height="100%"
+                    :src="result.imageURL"
+                  >
+                    <v-card-title class="font-weight-medium">{{
+                        result.name
+                    }}</v-card-title>
+                    <v-card-subtitle class="text-left"
+                      >{{ result.qtyDesc}}
+                      <br />
+                      {{"$" + result.price }}</v-card-subtitle
+                    >
+                  </v-img>
+                  <v-fade-transition>
+                    <v-overlay v-if="hover" absolute color="#fff">
+                      <v-btn color="#f5e4d0" class="black--text"
+                        >View Listing</v-btn
+                      >
+                    </v-overlay>
+                  </v-fade-transition>
+                </v-card>
+              </template>
+            </v-hover>
+          </v-col>
+        </v-row>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import db from "../firebase/firebaseInit";
+import firebase from "firebase/app";
 export default {
   name: "Landing",
+  data: () => ({
+    ListingResults: [],
+  }),
+  async mounted() {
+    const listings = await this.retrieveListings();
+    for (let i = 0; i < listings.length; i++) {
+      var ref = listings[i];
+      var imageURL = await this.retrieveImage(ref.imageRef);
+      listings[i]["imageURL"] = imageURL;
+    }
+    this.ListingResults = listings;
+  },
   computed: {
     isSeller() {
       return this.$store.state.isSeller;
+    },
+    getListings() {
+      return this.ListingResults;
+    },
+  },
+  methods: {
+    async retrieveListings() {
+      const docRef = db.collection("listings");
+      var listings = [];
+      await docRef.get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          listings.push({ ...doc.data(), docID: doc.id });
+        });
+      });
+      return listings;
+    },
+    async retrieveImage(imageRef) {
+      const storageRef = firebase.storage().ref();
+      var imageURL = "";
+      await storageRef
+        .child(imageRef)
+        .getDownloadURL()
+        .then((url) => {
+          console.log(url);
+          if (url) {
+            imageURL = url;
+          } else {
+            imageURL =
+              "https://cdn.shopify.com/s/files/1/0017/4699/3227/products/image_e0c99cb9-6dbf-427a-91b0-de7a3e115026_900x.jpg?v=1596376378";
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          imageURL =
+            "https://cdn.shopify.com/s/files/1/0017/4699/3227/products/image_e0c99cb9-6dbf-427a-91b0-de7a3e115026_900x.jpg?v=1596376378";
+        });
+      return imageURL;
     },
   },
 };
@@ -48,6 +145,7 @@ export default {
   background-size: cover;
   height: 92vh;
   text-align: center;
+  display: block;
 
   .landing-rectangle {
     padding: 50px;
@@ -58,6 +156,7 @@ export default {
     left: 10%;
     top: 10%;
     width: 30%;
+
     .supporting-header {
       font-size: 25px;
       text-align: center;
@@ -69,6 +168,19 @@ export default {
     background-color: #e89f71;
     color: #ffffff;
     font-weight: 600;
+  }
+  .div-title {
+    text-align: left;
+  }
+  .separator {
+    height: 39%;
+  }
+  .browse-new {
+    //margin-top: 220px;
+    background-color: #fffbf5;
+  }
+  .content-buyer {
+    margin: 0 5vw 0 5vw;
   }
 }
 </style>
